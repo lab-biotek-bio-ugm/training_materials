34222bf64db7c0f8db0672537dcbd1894b7be1dd
matinnuhamunada <m_nuhamunada@yahoo.co.id>
2019-09-01 05:53:35 +0700

    add html & py

---
+++ index.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <img src="images/Logo.png" style="width: 150px; float: right;">
+# 
+# ## Bioengineering Research Group
+# # Basic Bioinformatics Orientation
+# Selamat datang di [Bioengineering Research Group](https://matinnuhamunada.staff.ugm.ac.id), Laboratorium Bioteknologi, Fakultas Biologi, Universitas Gadjah Mada. Repository ini berisi materi pengantar bioinformatika bagi anggota lab sebagai dasar untuk mengerjakan project.
+
+# ## Daftar Topik
+
+# ### Instalasi Software
+# 
+
+# In[ ]:
+
+
+#### Lessons
+* [00. The command line and Linux (`terminal`)](00-platform/00-command-line.ipynb) - <font color='green'><b>introductory</b></font>
+* [01. Jupyter Notebooks (`notebook`)](00-platform/01-jupyter.ipynb) - <font color='green'><b>introductory</b></font>
+* [02. Python (`notebook`)](00-platform/02-python.ipynb) - <font color='green'><b>introductory</b></font>
+#### Learning Outcomes
+
+
+# In[ ]:
+
+
+
+
